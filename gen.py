ea13daece8366157c07171a96850f99dd93a4965
minttupyoneppo <minttupyoneppotrand@outlook.com>
2021-07-21 03:49:15 +0000

    fixUp

---
+++ gen.py
@@ -0,0 +1,34 @@
+import json, random, time, socket, platform
+
+timestr = time.strftime("%Y-%m-%d - %H:%M:%S UTC")
+
+
+
+f = open("./README.md", "w")
+
+f.write(f'''
+   
+#### This Page Create at:
+
+```bash
+
+{timestr}
+
+```
+
+
+```bash
+
+Host Name : {socket.gethostname()}
+
+platform  : {platform.platform()}
+
+Ip Local  : {socket.gethostbyname(socket.gethostname())}
+
+```
+
+
+
+''')
+
+f.close()
